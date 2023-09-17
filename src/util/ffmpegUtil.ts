186462de24dea91e56cdf62ad4e90b5ee3700404
zshnb <a857681664@gmail.com>
2023-09-17 20:10:46 +0800

    add options params in ffmpeg util

--- src/util/ffmpegUtil.ts
+++ src/util/ffmpegUtil.ts
@@ -1,11 +1,26 @@
 import { getLogger } from "../logger";
+import { Loglevel } from "../types/ffmpeg";
 import { execCommand } from "./childProcessUtil";
 
 const logger = getLogger('ffmpegUtil')
-export async function execFfmpeg(cmd: string) {
+export async function execFfmpeg(cmd: string, options: {
+  loglevel?: Loglevel
+  override?: boolean
+}) {
   const args = cmd.split(' ').slice(1)
+  function parseBasicParams() {
+    const args = []
+    options.loglevel === undefined ? 
+      args.push('error') :
+      args.push(options.loglevel)
+
+    if (options.override === true) {
+      args.push('-y')
+    }
+    return args
+  }
   await execCommand({
     cmd: 'ffmpeg',
-    params: ['-loglevel', 'error'].concat(args)
+    params: parseBasicParams().concat(args)
   })
 }
